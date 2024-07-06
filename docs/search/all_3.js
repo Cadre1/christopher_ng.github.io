36d8261ecb3c062e5bb077075f07d828ece934c1
Cadre1 <55156855+Cadre1@users.noreply.github.com>
2024-07-06 03:40:50 -0700

    Updated Resume

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['engineering_20competition_20caped_20project_0',['Ride Engineering Competition CAPED Project',['../ride_engineering_competition_project.html',1,'index']]],
-  ['engineering_20machinist_20engineering_20intern_1',['Ascentec Engineering Machinist/Engineering Intern',['../ascentec_engineering_internship.html',1,'index']]],
-  ['experience_2',['Experience',['../index.html#lab_sec',1,'Lab Experience'],['../index.html#work_sec',1,'Work Experience']]]
+  ['engineering_20intern_1',['Ascentec Engineering Machinist/Engineering Intern',['../ascentec_engineering_internship.html',1,'index']]],
+  ['engineering_20machinist_20engineering_20intern_2',['Ascentec Engineering Machinist/Engineering Intern',['../ascentec_engineering_internship.html',1,'index']]],
+  ['experience_3',['Experience',['../index.html#lab_sec',1,'Lab Experience'],['../index.html#work_sec',1,'Work Experience']]]
 ];
--- docs/search/pages_3.js
+++ docs/search/pages_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['engineering_20competition_20caped_20project_0',['Ride Engineering Competition CAPED Project',['../ride_engineering_competition_project.html',1,'index']]],
-  ['engineering_20machinist_20engineering_20intern_1',['Ascentec Engineering Machinist/Engineering Intern',['../ascentec_engineering_internship.html',1,'index']]]
+  ['engineering_20intern_1',['Ascentec Engineering Machinist/Engineering Intern',['../ascentec_engineering_internship.html',1,'index']]],
+  ['engineering_20machinist_20engineering_20intern_2',['Ascentec Engineering Machinist/Engineering Intern',['../ascentec_engineering_internship.html',1,'index']]]
 ];
